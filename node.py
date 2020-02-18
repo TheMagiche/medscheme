358846bbd635d548f13ee6d6dcadc2573412848a
STEVE <sngunyi998@gmail.com>
2020-02-17 19:34:05 -0600

    added login

--- node.py
+++ node.py
@@ -2,11 +2,46 @@ from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 from medscheme import *
+from functools import wraps
+from datetime import datetime, timedelta
+
+from flask import Blueprint, jsonify, request, current_app
+from config import Config
+from flask_migrate import Migrate
+import jwt
+
+from models import db, User
+
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 CORS(app)
 
+@app.route('/api/login/', methods=('POST',))
+def login():
+    data = request.get_json()
+    user = User.authenticate(**data)
 
+    if not user:
+        return jsonify({ 'message': 'Invalid credentials', 'authenticated': False }), 401
+
+    token = jwt.encode({
+        'sub': user.email,
+        'iat':datetime.utcnow(),
+        'exp': datetime.utcnow() + timedelta(minutes=30)},
+        current_app.config['SECRET_KEY'])
+    return jsonify({ 'token': token.decode('UTF-8') })
+
+@app.route('/api/register/', methods=('POST',))
+def register():
+    data = request.get_json()
+    user = User(**data)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.to_dict()), 201
 
 @app.route('/api/get-disease', methods=['POST'])
 def get_disease():
@@ -131,6 +166,38 @@ def get_specific_disease(disease):
         }
         return jsonify(response), 400 
 
+def token_required(f):
+    @wraps(f)
+    def _verify(*args, **kwargs):
+        auth_headers = request.headers.get('Authorization', '').split()
+
+        invalid_msg = {
+            'message': 'Invalid token. Registeration and / or authentication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+
+        if len(auth_headers) != 2:
+            return jsonify(invalid_msg), 401
+
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, current_app.config['SECRET_KEY'])
+            user = User.query.filter_by(email=data['sub']).first()
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401 # 401 is Unauthorized HTTP status code
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return _verify
+
 
 
 if __name__ == '__main__':
@@ -143,3 +210,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
     app.run(host='127.0.0.1', port=port)
+
